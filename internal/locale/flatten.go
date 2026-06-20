5164b3b397d92220f20d3e1921996a7c01492407
Ret2Hell <tayeb1920@gmail.com>
2026-06-20 12:02:32 +0100

    unsupported values shouldn't crash inventory

--- internal/locale/flatten.go
+++ internal/locale/flatten.go
@@ -20,6 +20,16 @@ func Flatten(ref FileRef, value any) FlattenResult {
 func flattenValue(ref FileRef, jsonPath []string, value any, result *FlattenResult) {
 	switch v := value.(type) {
 	case map[string]any:
+		if len(jsonPath) > 0 && looksLikeRichObject(v) {
+			result.Warnings = append(result.Warnings, newWarning(
+				ref,
+				"unsupported_rich_object",
+				"rich message objects are not supported as translation units in the MVP",
+				jsonPath,
+			))
+			return
+		}
+
 		keys := make([]string, 0, len(v))
 		for key := range v {
 			keys = append(keys, key)
@@ -37,7 +47,57 @@ func flattenValue(ref FileRef, jsonPath []string, value any, result *FlattenResu
 			FilePath:  ref.Path,
 			JSONPath:  append([]string(nil), jsonPath...),
 		})
+	case []any:
+		result.Warnings = append(result.Warnings, newWarning(
+			ref,
+			"unsupported_array",
+			"arrays are not supported as translation units in the MVP",
+			jsonPath,
+		))
+	case nil:
+		result.Warnings = append(result.Warnings, newWarning(
+			ref,
+			"unsupported_null",
+			"null values are not supported as translation units in the MVP",
+			jsonPath,
+		))
+	default:
+		result.Warnings = append(result.Warnings, newWarning(
+			ref,
+			"unsupported_non_string",
+			"non-string leaves are not supported as translation units in the MVP",
+			jsonPath,
+		))
 	}
+}
+
+func newWarning(ref FileRef, code string, message string, jsonPath []string) Warning {
+	return Warning{
+		Code:      code,
+		Message:   message,
+		Locale:    ref.Locale,
+		Namespace: ref.Namespace,
+		FilePath:  ref.Path,
+		Key:       keyFromPath(jsonPath),
+		JSONPath:  append([]string(nil), jsonPath...),
+	}
+}
+
+func looksLikeRichObject(value map[string]any) bool {
+	if _, ok := value["defaultMessage"]; ok {
+		return true
+	}
+	if _, ok := value["message"]; ok {
+		if _, hasDescription := value["description"]; hasDescription {
+			return true
+		}
+	}
+	if _, ok := value["values"]; ok {
+		if _, hasText := value["text"]; hasText {
+			return true
+		}
+	}
+	return false
 }
 
 func appendJSONPath(path []string, next string) []string {
